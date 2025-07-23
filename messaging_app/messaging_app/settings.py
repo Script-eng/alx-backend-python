fd4dabf4ed782d3ce3d1a9007eeea470d28266b4
Script-eng <lesalonsteven@gmail.com>
2025-07-23 21:52:21 +0300

    settings update

--- messaging_app/messaging_app/settings.py
+++ messaging_app/messaging_app/settings.py
@@ -37,6 +37,8 @@ INSTALLED_APPS = [
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
+    'django.rest_framework',
+    'messaging_app',  # Assuming this is the app name
 ]
 
 MIDDLEWARE = [
